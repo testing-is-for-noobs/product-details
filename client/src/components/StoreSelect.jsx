3b3a90dc76470bb87c1bcac35a8c2022f2cec0e7
Eric Kao <ucberickao@gmail.com>
2020-10-02 13:44:24 -0700

    Drop down menu icon repositioned

--- client/src/components/StoreSelect.jsx
+++ client/src/components/StoreSelect.jsx
@@ -6,15 +6,15 @@ const StoreSelect = ({ stores, toggleDrop, storeMenu }) => (
     <button type="button" className={styles.storeSelect} onClick={toggleDrop}>
       {storeMenu === 'minimized' && (
         <div className={`${styles.storeSelectArrow} ${styles.down}`}>
-          <svg width="18px" height="28px" viewBox="0 0 18 28" aria-hidden="true">
-            <path d="M1.825 28L18 14 1.825 0 0 1.715 14.196 14 0 26.285z" fill="#E0E0E0" />
+          <svg width="12px" height="19px" viewBox="0 0 18 28" aria-hidden="true">
+            <path d="M1.825 28L18 14 1.825 0 0 1.715 14.196 14 0 26.285z" fill="#757575" />
           </svg>
         </div>
       )}
       {storeMenu === 'expanded' && (
         <div className={`${styles.storeSelectArrow} ${styles.up}`}>
-          <svg width="18px" height="28px" viewBox="0 0 18 28" aria-hidden="true">
-            <path d="M1.825 28L18 14 1.825 0 0 1.715 14.196 14 0 26.285z" fill="#E0E0E0" />
+          <svg width="12px" height="19px" viewBox="0 0 18 28" aria-hidden="true">
+            <path d="M1.825 28L18 14 1.825 0 0 1.715 14.196 14 0 26.285z" fill="#757575" />
           </svg>
         </div>
       )}
